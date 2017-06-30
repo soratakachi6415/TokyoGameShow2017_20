8492aea96e017dd5961fd2006ddeee6c31738b76
ralsoon <smallkirby423@yahoo.co.jp>
2017-06-30 11:05:08 +0900

    LoadObject追加

--- HutonProto/Assets/takachi2/LoadObject.cs
+++ HutonProto/Assets/takachi2/LoadObject.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadObject : MonoBehaviour
 {
-    /*佐伯:追加*/
-    Scene_manager scenenmanager_;
-    public string curentscene;
-    /**********************/
 
     // Use this for initialization
     void Awake()
     {
         DontDestroyOnLoad(this);
+        SceneManager.sceneLoaded += SceneLoaded;
     }
 
-    void Start()
+    private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-        scenenmanager_ = GameObject.FindGameObjectWithTag("Scenemanager").GetComponent<Scene_manager>();
+        if (arg0.name == "Title")
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        curentscene = scenenmanager_.currentscene;
+
     }
 }
